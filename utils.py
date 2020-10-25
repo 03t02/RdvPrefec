fc705ab3a4d85455ea7194288c1a29d1bbce5ad8
Thanh TRAN <ttranpro32@gmail.com>
2020-10-25 19:20:00 +0100

    Load .env file

--- utils.py
+++ utils.py
@@ -1,6 +1,21 @@
+import os
+
+
 def key_exists(key: str, config: object):
     return key in config
 
 
 def key_not_exists(key: str, config: object):
     return key not in config
+
+
+def str_to_bool(var: str) -> bool:
+    return True if var == 'True' else False
+
+
+def env(var: str):
+    value = os.environ.get(var)
+
+    if value.capitalize() == 'True' or value.capitalize() == 'False':
+        return str_to_bool(value.capitalize())
+    return value
